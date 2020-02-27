0336268e194910b6c357d966271cf9b41e94b67a
Дмитрий <dima.aka.slogg@gmail.com>
2020-02-28 01:49:18 +0300

    refactoring drop trash

--- CurrencyWallet/CurrencyWallet/Startup.cs
+++ CurrencyWallet/CurrencyWallet/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CurrencyWallet.Domain.RateAPI;
+using CurrencyWallet.Domain.RateAPI.SpecificStrategy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,6 +32,7 @@ namespace CurrencyWallet
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            services.AddTransient(r => new RateContext(new EurofxrefStrategy()));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
