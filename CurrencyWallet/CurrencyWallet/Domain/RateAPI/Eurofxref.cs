9effae883334a70310dbe82ed64dd097b034e0c7
Дмитрий <dima.aka.slogg@gmail.com>
2020-02-27 22:19:15 +0300

    add rate strategy for the possibility of replacing api

---
+++ CurrencyWallet/CurrencyWallet/Domain/RateAPI/Eurofxref.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Xml;
+using CurrencyWallet.Models;
+
+namespace CurrencyWallet.Domain.RateAPI
+{
+    internal class Eurofxref : IRateStrategy
+    {
+        private string _adress = @"http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+
+        public IReadOnlyList<Rate> Update()
+        {
+            List<Rate> rates = new List<Rate>();
+
+            var doc = new XmlDocument();
+            doc.Load(_adress);
+
+            XmlNodeList nodes = doc.SelectNodes("//*[@currency]");
+
+            if (nodes != null)
+            {
+                rates = nodes.Cast<XmlNode>().Select(nd => new Rate
+                {
+                    Currency = nd.Attributes["currency"].Value,
+                    Value = decimal.Parse(nd.Attributes["rate"].Value)
+                }).ToList();
+            }
+            else
+            {
+                throw new NullReferenceException("Не удалось получить данные по валютам");
+            }
+
+            return rates;
+        }
+    }
+}
---
+++ CurrencyWallet/CurrencyWallet/Domain/RateAPI/IRateStrategy.cs
@@ -0,0 +1,13 @@
+﻿using CurrencyWallet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyWallet.Domain.RateAPI
+{
+    internal interface IRateStrategy
+    {
+        IReadOnlyList<Rate> Update();
+    }
+}
