86411ab85432853bd8b572012609811eb62ba889
Дмитрий <dima.aka.slogg@gmail.com>
2020-02-27 22:18:31 +0300

    add project

---
+++ CurrencyWallet/CurrencyWallet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurrencyWallet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
