f87c3e6490734d4264dfcaa53cdfdfec71be1ee6
Дмитрий <dima.aka.slogg@gmail.com>
2020-02-28 03:43:13 +0300

    add WalletController.cs

---
+++ CurrencyWallet/CurrencyWallet.Tests/Domain/WalletHandlingTests.cs
@@ -0,0 +1,48 @@
+﻿using CurrencyWallet.DAL;
+using CurrencyWallet.Domain;
+using CurrencyWallet.Models;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CurrencyWallet.Tests.Domain
+{
+    [TestFixture]
+    class WalletHandlingTests
+    {
+        private IWalletStore _walletStore;
+        private IRateValidate _rateValidate;
+        private IRateStore _rateStore;
+
+        [SetUp]
+        public void Setup()
+        {
+            _walletStore = new WalletStore();
+            _rateStore = Substitute.For<IRateStore>();
+            _rateStore.RateDict.Returns(new List<RateModel>
+            {
+                new RateModel(){Currency = "USD", Amount = 1.0964m},
+                new RateModel(){Currency = "BGN", Amount = 1.9558m}
+            });
+            _rateValidate = new RateValidate(_rateStore);
+        }
+
+        [TestCase("USD", 33)]
+        [TestCase("BGN", 3.4)]
+        public void ReplenishWallet_AddedNewCurrency_AddRowToListMoney(string currency, decimal amount)
+        {
+            string user = "пользователь1";
+            var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, user);
+            var rate = new RateModel() { Currency = currency, Amount = amount };
+
+            wh.ReplenishWallet(rate);
+
+            var amountStore = _walletStore.GetMoneyByUser(user).First(x => x.Currency.Equals(currency)).Amount;
+
+            Assert.AreEqual(amount, amountStore);
+        }
+
+    }
+}
---
+++ CurrencyWallet/CurrencyWallet/Controllers/WalletController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyWallet.DAL;
+using CurrencyWallet.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyWallet.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        private IWalletStore _walletStore;
+        private IRateValidate _rateValidate;
+        private IRateStore _rateStore;
+
+        public WalletController(IWalletStore walletStore, IRateStore rateStore, IRateValidate rateValidate)
+        {
+            _walletStore = walletStore;
+            _rateValidate = rateValidate;
+            _rateStore = rateStore;
+        }
+
+        /// <summary>
+        /// Получить все данные с кошельков пользователя
+        /// </summary>
+        /// <param name="userName">пользователь</param>
+        [HttpPost]
+        public async Task<IActionResult> GetAllMoneyUser(string userName)
+        {
+            try
+            {
+                var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, userName);
+                var result = wh.GetAllMoneyUser();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
