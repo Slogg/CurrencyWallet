d69da89ee8220dca0d90df3575ed8caa8a80662e
Дмитрий <dima.aka.slogg@gmail.com>
2020-02-28 04:39:45 +0300

    add api to controller

--- CurrencyWallet/CurrencyWallet/Controllers/WalletController.cs
+++ CurrencyWallet/CurrencyWallet/Controllers/WalletController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CurrencyWallet.DAL;
 using CurrencyWallet.Domain;
+using CurrencyWallet.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,13 +30,73 @@ namespace CurrencyWallet.Controllers
         /// </summary>
         /// <param name="userName">пользователь</param>
         [HttpPost]
-        public async Task<IActionResult> GetAllMoneyUser(string userName)
+        public IActionResult GetAllMoneyUser(string userName)
         {
             try
             {
                 var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, userName);
                 var result = wh.GetAllMoneyUser();
                 return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Снять деньги с кошелька
+        /// </summary>
+        /// <param name="userName">пользователь</param>
+        /// <param name="rate">валюта</param>
+        [HttpPost]
+        public IActionResult ReplenishWallet(string userName, RateModel rate)
+        {
+            try
+            {
+                var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, userName);
+                var result = wh.WithdrawModey(rate);
+                return Ok($"Сумма успешно зачислена. Остаток: {result} {rate.Currency}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Снять деньги с кошелька
+        /// </summary>
+        /// <param name="userName">пользователь</param>
+        /// <param name="rate">валюта</param>
+        [HttpPost]
+        public IActionResult WithdrawModey(string userName, RateModel rate)
+        {
+            try
+            {
+                var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, userName);
+                var result = wh.WithdrawModey(rate);
+                return Ok($"Сумма успешно снята. Остаток: {result} {rate.Currency}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Выполнить обмен валют
+        /// </summary>
+        /// <param name="userName">пользователь</param>
+        /// <param name="rateFrom">перевос с валюты</param>
+        /// <param name="rateTo">перевод на валюту</param>        
+        [HttpPost]
+        public IActionResult CurrencyExchange(string userName, RateModel rateFrom, RateModel rateTo)
+        {
+            try
+            {
+                var wh = new WalletHandling(_walletStore, _rateStore, _rateValidate, userName);
+                return Ok($"Обмен между валютами: {rateFrom.Currency} - {rateTo.Currency} прошёл успешно");
             }
             catch (Exception ex)
             {
